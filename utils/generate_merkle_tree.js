231cbefa989180cd9fc1484e931cd1962f56be17
Kuan-Hao Chen <35591407+nghdavid@users.noreply.github.com>
2024-06-08 14:48:31 +0800

    doc: Add Comments

--- utils/generate_merkle_tree.js
+++ utils/generate_merkle_tree.js
@@ -1,8 +1,7 @@
 const { StandardMerkleTree } = require('@openzeppelin/merkle-tree');
 const { fs } = require('fs');
 
-
-// (1)
+// Add or delete the addresses as needed
 const values = [
   ['0x5B38Da6a701c568545dCfcB03FcB875f56beddC4'],
   ['0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2'],
@@ -10,9 +9,9 @@ const values = [
   ['0x7F72dDF0e619F9B1600D9B68979BD5a3F21C01E7'],
 ];
 
-// (2)
 const tree = StandardMerkleTree.of(values, ['address']);
 
-// (3)
+// Intialize the NFT contract with tree.root
 console.log('Merkle Root:', tree.root);
+// Remember to move the tree.json file to the main folder
 fs.writeFileSync('tree.json', JSON.stringify(tree.dump()));
